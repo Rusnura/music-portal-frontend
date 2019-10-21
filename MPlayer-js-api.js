def979694ba8c09e4af118c661ad68f7991dd454
Ruslan Tumasov <Rus-nura@yandex.ru>
2019-10-22 00:02:39 +0700

    Updated js api

--- MPlayer-js-api.js
+++ MPlayer-js-api.js
@@ -51,15 +51,51 @@ class MPlayerAPI {
 	}
 	
 	login(login, password) {
-		let credentials = {
-			username: login,
-			password: password
-		};
-
-		this.webRequest.send(this.address + "api/authenticate", "POST", JSON.stringify(credentials), this.headers).then(function(data) {
-			this.token = data.token; // !!! CAN'T UPDATE TOKEN, error with "this" context
+		let _self = this;
+		return new Promise(function(resolve, reject) {
+			let credentials = {
+				username: login,
+				password: password
+			};
+			
+			_self.webRequest.send(_self.address + "api/authenticate", "POST", JSON.stringify(credentials), _self.headers).then(function(data) {
+				_self.token = data.token;
+				resolve(data);
+			}).catch(function(data) {
+				reject(data);
+			});
 		});
 	}
 	
+	createAlbum(name, description, internal) {
+		let albumData = {
+			name: name,
+			description: description,
+			internal: internal || false
+		};
+		
+		return this.webRequest.send(this.address + "api/album", "POST", JSON.stringify(albumData), {...this.headers, "Authorization": "Bearer " + this.token});
+	}
 	
+	getAlbum(albumId) {
+		return this.webRequest.send(this.address + "api/album/" + albumId, "GET", null, {...this.headers, "Authorization": "Bearer " + this.token});
+	}
+	
+	getUserAlbums(username) {
+		return this.webRequest.send(this.address + "api/user/" + username + "/albums", "GET", null, {...this.headers, "Authorization": "Bearer " + this.token});
+	}
+	
+	editAlbum(albumId, name, description, internal) {
+		let albumData = {
+			name: name,
+			description: description,
+			internal: internal || false
+		};
+		
+		return this.webRequest.send(this.address + "api/album/" + albumId, "PUT", JSON.stringify(albumData), {...this.headers, "Authorization": "Bearer " + this.token});
+	}
+	
+	deleteAlbum(albumId) {
+		return this.webRequest.send(this.address + "api/album/" + albumId, "DELETE", null, {...this.headers, "Authorization": "Bearer " + this.token});
+	}
 }
