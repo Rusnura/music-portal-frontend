7483dfc16369e89c35ede2584de6a1a192495627
Ruslan Tumasov <Rus-nura@yandex.ru>
2020-02-26 23:45:38 +0700

    - Released login page - WIP: Main page

--- js/login.js
+++ js/login.js
@@ -6,12 +6,18 @@ function doLogin() {
 	if (address && login && passw) {
 		let api = new MPlayerAPI(address);
 		api.login(login, passw).then(function(data) {
-			console.log("success data", data);
-			if (data.status == 200) {
-				document.location.reload();
+			if (data.token) {
+				localStorage.setItem("address", address);
+				localStorage.setItem("username", login);
+				localStorage.setItem("userpassw", passw);
+				document.location = "index.html";
 			}
 		}).catch(function(data) {
-			$("#login-info").html("<div class='alert alert-danger' role='alert'><strong>Login error</strong><br>" + data.responseJSON.message + "!</div>");
+			if (data.responseJSON) {
+				$("#login-info").html("<div class='alert alert-danger' role='alert'><strong>Login error</strong><br>" + data.responseJSON.message + "!</div>");
+			} else {
+				$("#login-info").html("<div class='alert alert-danger' role='alert'><strong>Login error</strong><br>Server error!</div>");
+			}
 		});
 	}
 }
--- js/system.js
+++ js/system.js
@@ -1,8 +1,32 @@
+var api = null;
+
 function init() {
 	__log("Initialization...");
-	__checkAuthentication();
+	__checkAuthentication().then(function() {
+		
+	}).catch(function() {
+		document.location = "login.html";
+	});
 }
 
 function __checkAuthentication() {
-	
+	return new Promise(function(resolve, reject) {
+		let address = localStorage.getItem("address");
+		let username = localStorage.getItem("username");
+		let userpass = localStorage.getItem("userpassw");
+		
+		try {
+			if (address && username && userpass) {
+				api = new MPlayerAPI(address);
+				api.login(username, userpass).then(function(data) {
+					if (data.token) {
+						resolve(true);
+					}
+				})
+			}
+		} catch {
+			// NOP
+		}
+		reject(false);
+	});
 }
