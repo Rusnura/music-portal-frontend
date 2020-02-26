5cb9f444dea9df0f7716b7c68758273ec948bbbf
Ruslan Tumasov <Rus-nura@yandex.ru>
2020-02-26 23:13:52 +0700

    - Added block for information when login fails - Added server address on login page

--- js/login.js
+++ js/login.js
@@ -1,8 +1,17 @@
 function doLogin() {
+	let address = $("#address").val();
 	let login = $("#login").val();
 	let passw = $("#password").val();
 	
-	if (login && passw) {
-		// TODO
+	if (address && login && passw) {
+		let api = new MPlayerAPI(address);
+		api.login(login, passw).then(function(data) {
+			console.log("success data", data);
+			if (data.status == 200) {
+				document.location.reload();
+			}
+		}).catch(function(data) {
+			$("#login-info").html("<div class='alert alert-danger' role='alert'><strong>Login error</strong><br>" + data.responseJSON.message + "!</div>");
+		});
 	}
 }
--- js/services/ConfigService.js
+++ js/services/ConfigService.js
@@ -1,11 +1,5 @@
 let __config = {};
 
 $(document).ready(function(){
-	_fetchConfigs();
+	
 });
-
-function _fetchConfigs() {
-	$.ajax("js/config.json", function(result) {
-		console.log(result);
-	});
-}
